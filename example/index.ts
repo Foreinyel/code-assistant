023d37c4054a407690e24db94d0e93d9dad97863
Foreinyel <Foreinyel@163.com>
2022-08-30 22:05:53 +0800

    0.0.2

--- example/index.ts
+++ example/index.ts
@@ -1,12 +1,12 @@
+import * as vscode from "vscode";
+
 const e = 3;
 export const a = (b) => {
   const bb = b + 1;
-  const { bbb, ccc, ddd } = fff({ bb });
-  return { bbb, ccc, ddd };
-};
-const fff = ({ bb }) => {
   const bbb = bb * 2;
   const ccc = bb * 3;
   const ddd = ccc + e;
+  const editor = vscode.window.activeTextEditor;
+
   return { bbb, ccc, ddd };
 };
--- src/extension.ts
+++ src/extension.ts
@@ -17,7 +17,7 @@ export function activate(context: vscode.ExtensionContext) {
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand(
-    "code-assistant.extract.toCurrentModule",
+    "jvs-code-assistant.extract.toCurrentModule",
     runCommand(extract.toCurrentModule)
   );
 
