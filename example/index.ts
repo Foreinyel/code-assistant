803bb5a4d4f2f5ce8cc2f8135dc88c52795d98d8
jimmy.shi <jimmy.shi@58.com>
2022-09-08 18:42:20 +0800

    example

--- example/index.ts
+++ example/index.ts
@@ -13,3 +13,27 @@ export const a = async (b) => {
 
   return { bbb, ccc, ddd };
 };
+
+export const isEmpty = (val) => val === undefined || val === null || val === "";
+
+function b(values, field, result) {
+  if (field in values) {
+    const value = values[field];
+    if (value) {
+      if (!isEmpty(value.min) || !isEmpty(value.max)) {
+        result[field] = {
+          lower: value.min,
+          upper: value.max,
+        };
+      } else if (!!value?.range?.lower || !!value?.range?.upper) {
+        result[field] = {
+          ...value?.range,
+        };
+      } else {
+        result[field] = undefined;
+      }
+    } else {
+      result[field] = undefined;
+    }
+  }
+}
