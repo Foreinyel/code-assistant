9edf5098e92ade1b876e3df1230636e0d7128746
Foreinyel <Foreinyel@163.com>
2022-09-26 21:45:49 +0800

    env.js

---
+++ env.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const pkgJson = require("./package.json");
+const path = require("path");
+
+const [mode] = process.argv.slice(2);
+
+const main = () => {
+  if (mode === "dev") {
+    pkgJson.main = "./out/extension.js";
+  } else if (mode === "prod") {
+    pkgJson.main = "./bundle/main.js";
+  }
+  fs.writeFileSync(
+    path.resolve(__dirname, "./package.json"),
+    JSON.stringify(pkgJson)
+  );
+};
+
+main();
