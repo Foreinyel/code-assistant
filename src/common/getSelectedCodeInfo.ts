04cccd59e3f0393d37d1ef2feee80e31d6ef4ea7
Foreinyel <Foreinyel@163.com>
2022-11-11 22:46:25 +0800

    getSelectedCodeInfo

--- src/common/getSelectedCodeInfo.ts
+++ src/common/getSelectedCodeInfo.ts
@@ -1,8 +1,94 @@
-import * as vscode from "vscode";
 import * as doctor from "@fe-doctor/core";
 import { strict as assert } from "assert";
-import { getDocumentInfo } from "./getDocumentInfo";
 import ts from "typescript";
+import { getDocumentInfo } from "./getDocumentInfo";
+const checkIfNodeEndToLastLine = (
+  lineStart: any,
+  lineEnd: any,
+  node: any,
+  characterEnd: any,
+  characterStart: any
+) => {
+  return (
+    lineStart !== lineEnd &&
+    node?.line?.end === lineEnd &&
+    node.character.end <= characterEnd &&
+    (node?.line?.start === lineStart
+      ? node?.character.start >= characterStart
+      : node?.line?.start < lineStart
+      ? false
+      : true)
+  );
+};
+const checkIfNodeInOneLine = (
+  lineStart: any,
+  lineEnd: any,
+  node: any,
+  characterStart: any,
+  characterEnd: any
+) => {
+  return (
+    lineStart === lineEnd &&
+    node?.line?.start === lineStart &&
+    node?.line?.end === lineEnd &&
+    node?.character?.start >= characterStart &&
+    node.character.end <= characterEnd
+  );
+};
+const checkIfNodeInInnerLines = (node: any, lineStart: any, lineEnd: any) => {
+  return node?.line?.start > lineStart && node?.line?.end < lineEnd;
+};
+const checkIfNodeStartFromFirstLine = (
+  lineStart: any,
+  lineEnd: any,
+  node: any,
+  characterStart: any,
+  characterEnd: any
+) => {
+  return (
+    lineStart !== lineEnd &&
+    node?.line?.start === lineStart &&
+    node?.character?.start >= characterStart &&
+    (node?.line?.end === lineEnd
+      ? node?.character?.end <= characterEnd
+      : node?.line?.end > lineEnd
+      ? false
+      : true)
+  );
+};
+const checkIfNodeSelected = (
+  node: any,
+  lineStart: any,
+  lineEnd: any,
+  characterStart: any,
+  characterEnd: any
+) => {
+  return (
+    checkIfNodeInInnerLines(node, lineStart, lineEnd) ||
+    checkIfNodeStartFromFirstLine(
+      lineStart,
+      lineEnd,
+      node,
+      characterStart,
+      characterEnd
+    ) ||
+    checkIfNodeEndToLastLine(
+      lineStart,
+      lineEnd,
+      node,
+      characterEnd,
+      characterStart
+    ) ||
+    checkIfNodeInOneLine(lineStart, lineEnd, node, characterStart, characterEnd)
+  );
+};
+const checkIfParentAndChildrenSameRange = (node: any) => {
+  return (
+    node.sons.length === 1 &&
+    node.sons[0].kind === ts.SyntaxKind.Identifier &&
+    node.kind === ts.SyntaxKind.BindingElement
+  );
+};
 export const getSelectedCodeInfo = () => {
   const {
     editor,
@@ -34,45 +120,24 @@ export const getSelectedCodeInfo = () => {
       typeof node?.line?.end === "number" &&
       typeof node?.character?.start === "number" &&
       typeof node?.character?.end === "number" &&
-      ((node?.line?.start > lineStart && node?.line?.end < lineEnd) ||
-        (lineStart !== lineEnd &&
-          node?.line?.start === lineStart &&
-          node?.character?.start >= characterStart &&
-          (node?.line?.end === lineEnd
-            ? node?.character?.end <= characterEnd
-            : node?.line?.end > lineEnd
-            ? false
-            : true)) ||
-        (lineStart !== lineEnd &&
-          node?.line?.end === lineEnd &&
-          node.character.end <= characterEnd &&
-          (node?.line?.start === lineStart
-            ? node?.character.start >= characterStart
-            : node?.line?.start < lineStart
-            ? false
-            : true)) ||
-        (lineStart === lineEnd &&
-          node?.line?.start === lineStart &&
-          node?.line?.end === lineEnd &&
-          node?.character?.start >= characterStart &&
-          node.character.end <= characterEnd))
+      checkIfNodeSelected(
+        node,
+        lineStart,
+        lineEnd,
+        characterStart,
+        characterEnd
+      )
     ) {
       nodeIdsInSelectedNodes.add(node.id);
       selectedNodes.push(node);
     }
   }
-
   const selectedNodesValid = selectedNodes.filter((node) => {
-    if (
-      node.sons.length === 1 &&
-      node.sons[0].kind === ts.SyntaxKind.Identifier &&
-      node.kind === ts.SyntaxKind.BindingElement
-    ) {
+    if (checkIfParentAndChildrenSameRange(node)) {
       return false;
     }
     return true;
   });
-
   return {
     nodeList,
     nodeIdsInSelectedNodes,
