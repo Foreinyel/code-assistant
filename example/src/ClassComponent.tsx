e2986610617e13b22dda93fecc0daaab0186194f
jimmy.shi <jimmy.shi@58.com>
2022-12-06 18:23:18 +0800

    rename identifier

--- example/src/ClassComponent.tsx
+++ example/src/ClassComponent.tsx
@@ -2,20 +2,22 @@ import React from "react";
 import { Button } from "antd";
 export default class ClassComponent extends React.Component<any, any> {
   state = {
-    a: 1,
+    aaa: 1,
   };
   constructor(props) {
     super(props);
   }
-  setAA = () => {
+  setAAA = () => {
     this.setState({ a: 3 });
   };
   render() {
+    const { aaa } = this.state;
+    console.log(`🚀 ~ file: ClassComponent.tsx:15 ~ ClassComponent ~ render ~ aaa`, aaa);
     return (
       <div>
-        <div>{this.state.a}</div>
+        <div>{this.state.aaa}</div>
         <div>{this.props.name}</div>
-        <Button onClick={this.setAA}>点击</Button>
+        <Button onClick={this.setAAA}>点击</Button>
       </div>
     );
   }
