d055d94bbfcbe51a2cdba26f45bf796b92501c4c
jimmy.shi <jimmy.shi@58.com>
2022-12-19 19:27:40 +0800

    class component

--- example/src/ClassComponent.tsx
+++ example/src/ClassComponent.tsx
@@ -1,24 +1,23 @@
 import React from "react";
 import { Button } from "antd";
 export default class ClassComponent extends React.Component<any, any> {
-  state = {
-    aaa: 1,
-  };
-  constructor(props) {
-    super(props);
-  }
-  setAAA = () => {
-    this.setState({ a: 3 });
-  };
-  render() {
-    const { aaa } = this.state;
-    console.log(`🚀 ~ file: ClassComponent.tsx:15 ~ ClassComponent ~ render ~ aaa`, aaa);
-    return (
-      <div>
-        <div>{this.state.aaa}</div>
+    state = {
+        opop: 1,
+    };
+    constructor(props) {
+        super(props);
+    }
+    setAAA = () => {
+        this.setState({ a: 3 });
+    };
+    render() {
+        const { state: ssss } = this;
+        const { opop: vvvv } = this.state;
+        console.log(`🚀 ~ file: ClassComponent.tsx:15 ~ ClassComponent ~ render ~ aaa`, vvvv, ssss.opop);
+        return (<div>
+        <div>{this.state.opop}</div>
         <div>{this.props.name}</div>
         <Button onClick={this.setAAA}>点击</Button>
-      </div>
-    );
-  }
+      </div>);
+    }
 }
