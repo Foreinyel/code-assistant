4d9f1a00d9b27ac8e90e80e47aed1fe22132f643
jimmy.shi <jimmy.shi@58.com>
2022-12-20 17:28:15 +0800

    renameIdentifierInModule

--- example/src/ClassComponent.tsx
+++ example/src/ClassComponent.tsx
@@ -1,23 +1,26 @@
 import React from "react";
 import { Button } from "antd";
 export default class ClassComponent extends React.Component<any, any> {
-    state = {
-        opop: 1,
-    };
-    constructor(props) {
-        super(props);
-    }
-    setAAA = () => {
-        this.setState({ a: 3 });
-    };
-    render() {
-        const { state: ssss } = this;
-        const { opop: vvvv } = this.state;
-        console.log(`🚀 ~ file: ClassComponent.tsx:15 ~ ClassComponent ~ render ~ aaa`, vvvv, ssss.opop);
-        return (<div>
-        <div>{this.state.opop}</div>
+  state = {
+    mmmm: 1,
+  };
+  constructor(props) {
+    super(props);
+    this.state.mmmm = 3;
+  }
+  setAAA = () => {
+    this.setState({ mmmm: 3 });
+  };
+  render() {
+    const { state: sssssss } = this;
+    const { mmmm: ccdsd } = this.state;
+    console.log(`🚀 ~ file: ClassComponent.tsx:15 ~ ClassComponent ~ render ~ aaa`, ccdsd, sssssss.mmmm);
+    return (
+      <div>
+        <div>{this.state.mmmm}</div>
         <div>{this.props.name}</div>
         <Button onClick={this.setAAA}>点击</Button>
-      </div>);
-    }
+      </div>
+    );
+  }
 }
